4b3fa529a2eefb0d09703b8abbcfbe4d7cea9d3e
mlfernandez <mlfernandez@gmail.com>
2021-07-26 20:57:27 +0200

    draft login to test navbar, navbar almost ok

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import Logo from '../../images/afterlogo.png';
-import PhotoProfile from '../../images/afterlogo.png';
-import { NavLink } from 'react-router-dom';
+import {Input, notification} from 'antd';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/js/dist/dropdown';
+import './Navbar.scss';
 import { connect } from 'react-redux';
-import { LOGOUT } from '../../redux/types';
+import { LOGOUT, PROFILE } from '../../redux/types';
 /* import { LOGOUT, LOGOUTTIPODATOS, PROFILE, DELETE, FAQ} from '../../redux/types'; */
 import { useHistory } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './Navbar.scss';
-/* import {Input, notification} from 'antd'; */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 
 const Navbar = (props) => {
 
@@ -26,15 +29,19 @@ const Navbar = (props) => {
 
     let mensaje = "Hasta pronto " + props.credentials.user.name
 
-/*     notification.success({message:'Logout correcto.',description: mensaje}); */
+    notification.success({message:'¡Hasta luego!',description: mensaje});
 
     props.dispatch({ type: LOGOUT });
     
 /*     props.dispatch({ type: LOGOUTTIPODATOS });
     props.dispatch({ type: DELETE }); */
 
-    setTimeout(() => {
+/*     setTimeout(() => {
       history.push('/datacontainer');
+    }, 1) */
+
+    setTimeout(() => {
+      history.push('/');
     }, 1)
 
   }
@@ -47,10 +54,10 @@ const Navbar = (props) => {
 
             break;
  */
-/*         case "profile":
-            props.dispatch({ type: PROFILE, payload: info });
+        case "profile":
+        /*     props.dispatch({ type: PROFILE, payload: info }); */
 
-            break; */
+            break;
 
       default:
 
@@ -70,15 +77,13 @@ const Navbar = (props) => {
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse">
-                    <a class="navbar-brand mlf-logo-bracket" href="#">[</a>
-                    <a class="navbar-brand mlf-logo" href="#">AFTER</a>
-                    <a class="navbar-brand mlf-logo-bracket" href="#">]</a>
+                    <a class="navbar-brand mlf-logo-bracket" href="/">[</a>
+                    <a class="navbar-brand mlf-logo" href="/">AFTER</a>
+                    <a class="navbar-brand mlf-logo-bracket" href="/">]</a>
                 </div>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active mlf-more-space" aria-current="page" href="#">Home</a>
-                        </li>
+
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Categorías
@@ -88,15 +93,16 @@ const Navbar = (props) => {
                                 <li><a class="dropdown-item" href="#">Tecnología</a></li>
                                 <li><a class="dropdown-item" href="#">Management</a></li>
                             </ul>
+                            
                         </li>
                         <form class="d-flex">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button class="btn btn-outline-success me-2 mlf-btn-color" type="submit">Buscar</button>
+                            <button class="btn btn-outline-dark me-2 mlf-btn-color" type="submit" >Buscar</button>
                         </form>
                     </ul>
                         <form class="d-flex justify-content-end">
-                            <button class="btn btn-outline-success me-2 mlf-btn-color" type="button">Inicia sesión</button>
-                            <button class="btn btn-outline-success me-2 mlf-btn-color-solid" type="button">Regístrate</button>
+                            <button class="btn btn-outline-dark me-2 mlf-btn-color" type="button" onClick={() => history.push('/login')}>Inicia sesión</button>
+                            <button class="btn btn-outline-dark me-2 mlf-btn-color-solid" type="button" onClick={() => history.push('/register')}>Regístrate</button>
                         </form>
                
                 </div>
@@ -130,6 +136,47 @@ const Navbar = (props) => {
     // vista navbar usuario logeado
   } else {
     return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse">
+              <a class="navbar-brand mlf-logo-bracket" href="/">[</a>
+              <a class="navbar-brand mlf-logo" href="/">AFTER</a>
+              <a class="navbar-brand mlf-logo-bracket" href="/">]</a>
+          </div>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Categorías
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <li><a class="dropdown-item" href="#">Negocios</a></li>
+                          <li><a class="dropdown-item" href="#">Tecnología</a></li>
+                          <li><a class="dropdown-item" href="#">Management</a></li>
+                      </ul>
+                      
+                  </li>
+                  <form class="d-flex">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                      <button class="btn btn-outline-dark me-2 mlf-btn-color" type="submit" >Buscar</button>
+                  </form>
+              </ul>
+                  <form class="d-flex justify-content-end">
+                      <button class="btn btn-outline-dark me-2 mlf-btn-color" type="button"  onClick={()=>logOut()} to="/">Cerrar sesión</button>
+                      <button class="btn btn-outline-dark me-2 mlf-btn-color" type="button"  onClick={()=> cambiaDatos('projects')} >Mis proyectos</button>
+                      <button class="btn btn-outline-dark me-2 mlf-btn-color-solid" type="button" onClick={() => history.push('/profile')}>
+                        <div className="fotoUser"><FontAwesomeIcon className="iconMenuLateral" onClick={() => cambiaDatos("profile")} icon={faUser}/> Perfil</div>
+                      </button>
+                  </form>
+         
+          </div>
+      </div>
+  </nav>
+  /* 
       <div className="nav">
         <div className="logo">
           <NavLink to="/"><img className="img" src={Logo} alt={"logo"}/></NavLink>
@@ -154,7 +201,7 @@ const Navbar = (props) => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */
         )
     }
 };
